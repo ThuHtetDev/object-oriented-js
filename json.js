c95f34b47a39900d7c2cf86b9de4f08600b81ca1
ThuhtetCoder <59603198+ThuhtetCoder@users.noreply.github.com>
2020-09-30 22:56:35 +0630

    object oriented js

--- json.js
+++ json.js
@@ -1,1 +1,61 @@
-console.log("this is json file")
+console.log("Object Oriented Js")
+
+// ! object
+const car = {
+    id: 1,
+    name: 'lambo',
+    model: 2019,
+    drive(){
+      return 'move like rock'
+    }
+}
+console.log(car.drive());
+
+// ! Class
+
+// 1 create an class
+class Car{
+    // 2 constructor
+    constructor(name,model = null){
+        this.name = name;
+        this.model = model;
+    }
+    // 3 class methods
+    getInfo(){
+         console.log(`This Car is ${this.name} and its model is ${this.model}`)
+         return this;
+    }
+    getChain(){
+        console.log('method chaining')
+    }
+}
+
+let one = new Car('lambo'); // create an object
+let two = new Car('Suzuki','2020');
+console.log(one);
+console.log(two)
+
+one.getInfo();
+two.getInfo().getChain(); // Method chaining
+
+// ! class inheritance
+class SuperCar extends Car {
+    getSuperCar(){
+        console.log(`I am super car called ${this.name}`)
+    }
+}
+
+let supercar = new SuperCar('audi','2000');
+supercar.getSuperCar();
+
+// ! Contructor Function
+
+function User(name){
+    this.name = name;
+    this.login = function(){
+        console.log(`${this.name} is logged in now`)
+    }
+}
+let uone = new User('thu htet');
+console.log(uone);
+uone.login();
